86e3a69fde6d05b120ff325912148b9818013ade
nvinas <neil.vinas@gmail.com>
2018-02-28 14:34:12 +0800

    moved fragment to activity

--- app/src/main/java/com/nvinas/hnews/ui/stories/StoriesModule.java
+++ app/src/main/java/com/nvinas/hnews/ui/stories/StoriesModule.java
@@ -3,16 +3,12 @@ package com.nvinas.hnews.ui.stories;
 
 import dagger.Binds;
 import dagger.Module;
-import dagger.android.ContributesAndroidInjector;
 
 /**
  * Created by nvinas on 10/02/2018.
  */
 @Module
 public abstract class StoriesModule {
-
-    @ContributesAndroidInjector
-    abstract StoriesFragment storiesFragment();
 
     @Binds
     abstract StoriesContract.Presenter storiesPresenter(StoriesPresenter storiesPresenter);
